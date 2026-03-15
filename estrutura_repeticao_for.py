520f83b18f1a49aaaa73660be13a4daa6fc66024
Claudio Neves <cfneguacu@gmail.com>
2026-03-15 20:35:35 -0300

    Estrutura repetição for

---
+++ estrutura_repeticao_for.py
@@ -0,0 +1,8 @@
+texto = input("informe um texto:")
+VOGAIS = "AEIOU"
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end="")
+else:
+    print()
+    print("Executa no final do laço")
